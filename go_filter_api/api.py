c2fec82d487af176c9643654cd36e81d990f77f0
Jihun <81672004+HonorJay@users.noreply.github.com>
2022-12-18 17:50:00 +0900

    Add files via upload

--- go_filter_api/api.py
+++ go_filter_api/api.py
@@ -1,10 +1,19 @@
 from typing import Dict
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from model import Model, get_model
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 class ClassifierRequest(BaseModel):
     sentence: str
@@ -12,9 +21,13 @@ class ClassifierRequest(BaseModel):
 class ClassifierResponse(BaseModel):
     result : str
 
-@app.get("/inference", response_model=ClassifierResponse)
+@app.post("/inference/", response_model=ClassifierResponse)
 def inference(request: ClassifierRequest, model: Model = Depends(get_model)):
-    result = model.inference(request.text)
+    result = model.inference(request.sentence)
     return ClassifierResponse(
         result = result
     )
+    
+@app.get('/')
+def root():
+    return {'hello': 'world'}
