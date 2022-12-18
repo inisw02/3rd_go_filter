df347373324075834d6b878fc55ba6662213e669
Jihun <81672004+HonorJay@users.noreply.github.com>
2022-12-18 17:57:12 +0900

    Update api.py

--- go_filter_api/api.py
+++ go_filter_api/api.py
@@ -1,6 +1,5 @@
 from typing import Dict
 from fastapi import Depends, FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from model import Model, get_model
